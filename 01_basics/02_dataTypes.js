3e989809aaf64eedf895d21e9a05bb702c755165
Varsha Gupta <vrsa8896@gmail.com>
2024-07-08 19:45:15 +0530

    ..

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+'use strict'; //treat all JS code as newer version
+
+let name =  "Varsha";
+let age = 25;
+let isLoggedIn = false;
+
+//number => 2 to power53
+//bigint
+// string
+//boolean => tru/false
+//null => standalone value
+//undefined => value not assign
+//symbol = find to unique
+
+//object
+
+console.log(typeof "varsha");
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+
+//type of datatype
+ 
+
+// primitive
+//7 Types - : String, Number,  Boolean, null, undefined, Symbol, BigInt
+
+//non primitive - Refrence type
+//// Array, Object, Function
+
+//JavaScript is dynamic type 
---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,11 @@
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" = 33
+//"33ab" = NAN
